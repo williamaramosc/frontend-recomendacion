b3818e7d252c6ed40ec398d30a59b155237bf531
Hernan Enrique Gordillo Garzòn <Hernan Gordillo>
2021-12-01 22:20:20 -0500

    Version sin nada.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,11 @@
  */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './pages/inicio/inicio.component';
-import { EmpleadosComponent } from './pages/empleados/empleados.component';
 
 /**
  * Declaración e inicialización de la constante routes
  */
 const routes: Routes = [
-  {path: 'inicio', component: InicioComponent},
-  {path: 'empleados', component: EmpleadosComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'inicio'},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,16 +24,14 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
-import { EmpleadosComponent } from './pages/empleados/empleados.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    InicioComponent,
-    EmpleadosComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
